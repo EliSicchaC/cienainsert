c0cd62f5c9d6033656691ce5df0323ec85d7c4c0
reibin quisquiche   siccha <reibinsql@gmail.com>
2021-12-14 15:36:21 -0500

    agregan cambios modelo

--- src/main/java/com/ciena/controller/OwnedNodePoint.java
+++ src/main/java/com/ciena/controller/OwnedNodePoint.java
@@ -14,123 +14,114 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 public class OwnedNodePoint {
-    private static Conexion.DBConnector dataBase;
-    private static DBTable tablaOwnedNode;
-    private static DBTable tablaDiccionario;
+	private static Conexion.DBConnector dataBase;
+	private static DBTable tablaOwnedNode;
+	private static DBTable tablaDiccionario;
 
-    //Se crea un metodo main, para poder ejecutarlo y tambien conectarnos al json
-    public static void main(final String[] args) throws IOException {
-        OwnedNodePoint mainTopology = new OwnedNodePoint();
-        mainTopology.analizarInformacionTopoloyContext("D:\\archivos\\objetociena.json","tapi-common:context","tapi-topology:topology-context",
-                "topology","node","owned-node-edge-point");
-    }
-    public Boolean analizarInformacionTopoloyContext(String rutaDeArchivo,String tapiContext,String tapiTopology,
-                                                     String topology, String node,String ownedPoint){
-        Boolean analizo = false;
-        System.out.println("--------------------IMPRIMIR topology-------- \n");
-        OwnedNodePoint mainTopology = new OwnedNodePoint();
-        try {
-            dataBase = new Conexion.DBConnector();
-            JSONObject contenidoObjetosTotales = Util.parseJSONFile(rutaDeArchivo);
-            JSONObject objetoTopologyContext = Util.retonarListaPropiedadesAsociadasNodoHijo(contenidoObjetosTotales,tapiContext,tapiTopology);
+	// Se crea un metodo main, para poder ejecutarlo y tambien conectarnos al json
+	public static void main(final String[] args) throws IOException {
+		OwnedNodePoint mainTopology = new OwnedNodePoint();
+		mainTopology.analizarInformacionTopoloyContext("D:\\archivos\\objetociena.json", "tapi-common:context",
+				"tapi-topology:topology-context", "topology", "node", "owned-node-edge-point");
+	}
 
+	public Boolean analizarInformacionTopoloyContext(String rutaDeArchivo, String tapiContext, String tapiTopology,
+			String topology, String node, String ownedPoint) {
+		boolean analizo = false;
+		boolean insertoDiccionarioOwned = false;
+		boolean insertoMatrizOwned = false;
+		System.out.println("-------------Procesando información de: " + ownedPoint + "------- \n");
+		try {
+			dataBase = new Conexion.DBConnector();
+			JSONObject contenidoObjetosTotales = Util.parseJSONFile(rutaDeArchivo);
+			JSONObject objetoTopologyContext = Util.retonarListaPropiedadesAsociadasNodoHijo(contenidoObjetosTotales,
+					tapiContext, tapiTopology);
+			JSONArray topologyArray = objetoTopologyContext.getJSONArray(topology);
+			JSONObject ownedNode = (JSONObject) topologyArray.get(0);
+			JSONArray listaDeNode = ownedNode.getJSONArray(node);
 
-            JSONArray topologyArray = objetoTopologyContext.getJSONArray(topology);
-            // QUE ME TRAIGA EL PRIMER OBJETO
-            JSONObject ownedNode = (JSONObject) topologyArray.get(0);
-            JSONArray listaDeNode = ownedNode.getJSONArray(node);
+			List<String> listaColumnas = Util.listaDeColumnasPadreArray(listaDeNode, ownedPoint);
+			insertoDiccionarioOwned = insertarDiccionarioOwned(listaColumnas, dataBase);
+			insertoMatrizOwned = insertarMatrizOwnedNode(listaColumnas, dataBase, listaDeNode, ownedPoint);
+			System.out.println("-------------Procesando ejecutado con exito: " + insertoDiccionarioOwned  + "/ "+ insertoMatrizOwned);
+			analizo = insertoDiccionarioOwned && insertoMatrizOwned ? true : false;
+		} catch (Exception e) {
+			analizo = false;
+			System.out.println("-------------Procesando con errores: " + e.getMessage());
+			e.printStackTrace();
+		}
 
-            List<String> listaColumnas = Util.listaDeColumnasPadreArray(listaDeNode,ownedPoint);
-            boolean insertoDiccionarioOwned = insertarDiccionarioOwned(listaColumnas,dataBase);
-            boolean insertoMatrizOwned = insertarMatrizOwnedNode(listaColumnas,dataBase,listaDeNode,ownedPoint);
-            analizo = true;
-        } catch (Exception e) {
-            analizo = false;
-            e.printStackTrace();
-        }
-        return analizo;
-    }
+		return analizo;
+	}
 
-    private Boolean insertarDiccionarioOwned(List<String> listaDeColumnas, Conexion.DBConnector dataBase){
-        String[][] ownedpoint = new String[][]{
-                {
-                        "id","int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY"
-                },
-                {
-                        "atribute_name","varchar(250)"
-                },
-                {
-                        "sample_reference_object_uuid","varchar(250)"
-                },
-                {
-                        "sample_value","MEDIUMTEXT"
-                },
-                {
-                        "sample_reference_onep_layer_protocol_qualifier","MEDIUMTEXT"
-                }
-        };
-        listaDeColumnas = listaDeColumnas.stream().distinct().collect(Collectors.toList());
-        try {
-            tablaDiccionario = Util.crearTablasGenerico(dataBase,"dic_topology_onep",tablaDiccionario,ownedpoint);
-            DBRecord recorre = tablaDiccionario.newRecord();
-            for(String objetos : listaDeColumnas){
-                recorre = tablaDiccionario.newRecord();
-                try {
-                    recorre.addField("atribute_name", objetos);
-                    tablaDiccionario.insert(recorre);
+	private Boolean insertarDiccionarioOwned(List<String> listaDeColumnas, Conexion.DBConnector dataBase) {
+		String[][] ownedpoint = new String[][] { { "id", "int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY" },
+				{ "atribute_name", "varchar(250)" }, { "sample_reference_object_uuid", "varchar(250)" },
+				{ "sample_value", "MEDIUMTEXT" }, { "sample_reference_onep_layer_protocol_qualifier", "MEDIUMTEXT" } };
+		listaDeColumnas = listaDeColumnas.stream().distinct().collect(Collectors.toList());
+		try {
+			String nombreTabla = "dic_topology_onep";
+			System.out.println("	-------------Creando tabla: " + nombreTabla);
+			tablaDiccionario = Util.crearTablasGenerico(dataBase, nombreTabla, tablaDiccionario, ownedpoint);
+			DBRecord recorre = tablaDiccionario.newRecord();
+			for (String objetos : listaDeColumnas) {
+				recorre = tablaDiccionario.newRecord();
+				recorre.addField("atribute_name", objetos);
+				tablaDiccionario.insert(recorre);
 
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-        } catch (SQLException | ClassNotFoundException e) {
-            return false;
-        }
-        return true;
+			}
+		} catch (SQLException | ClassNotFoundException e) {
+			return false;
+		}
+		return true;
 
-    }
-    private Boolean insertarMatrizOwnedNode(List < String > listaDeColumnas, Conexion.DBConnector dataBase, JSONArray nodoEvaluar, String nodoHijoInsertarData) {
-        Map < String, String > tablaOwned = new HashMap();
-        for (String objectos: listaDeColumnas) {
-            //INSERTANDO DATA
-            String nombreColumna = objectos.replaceAll("-", "_").replaceAll(":", "_");
-            if(nombreColumna.equals("uuid")){
-                tablaOwned.put(nombreColumna, "varchar(50) primary key");
-            }else{
-                tablaOwned.put(nombreColumna,"MEDIUMTEXT");
-            }
-        }
-        tablaOwned.put("uuid_node", "varchar(250) , FOREIGN KEY  (uuid_node) REFERENCES exp_topology_node(uuid)");
-        try {
-            tablaOwnedNode = Util.crearTablasGenericoMap(dataBase, "exp_topology_owned_node_edgepoint", tablaOwnedNode, tablaOwned);
-            DBRecord record = tablaOwnedNode.newRecord();
-            for (Object objetosNode: nodoEvaluar) {
-                //JSONOBJECT ES PARA TRAER UN OBJETO DEL JSON
-                JSONObject ownedNode = (JSONObject) objetosNode;
-                //QUIERO ATRAER EL UUID DE OWNEDNODE PARA LUEGO IMPLEMENTARLO EN LA BD
-                String columnaUuid = ownedNode.get("uuid").toString();
-                JSONArray listEdgePoint = ownedNode.getJSONArray(nodoHijoInsertarData);
-                for (Object objectEvaluado: listEdgePoint) {
-                    JSONObject objetosEvaluadoDeJson = (JSONObject) objectEvaluado;
-                    Map < String, Object > objetosMap = objetosEvaluadoDeJson.toMap();
-                    record = tablaOwnedNode.newRecord();
-                    for (Map.Entry < String, Object > entry: objetosMap.entrySet()) {
-                        if (listaDeColumnas.stream().filter(x -> entry.getKey().equals(x)).findFirst().isPresent()) {
-                            record.addField(entry.getKey().replaceAll("-", "_").replaceAll(":", "_"), entry.getValue().toString());
-                        } else {
-                            record.addField(entry.getKey().replaceAll("-", "_").replaceAll(":", "_"), null);
-                        }
-                    }
-                    record.addField("uuid_node", columnaUuid);
-                    tablaOwnedNode.insert(record);
+	}
 
-                }
-            }
-        } catch (SQLException | ClassNotFoundException e) {
-            e.printStackTrace();
-            return false;
-        }
-        return true;
-    }
+	private Boolean insertarMatrizOwnedNode(List<String> listaDeColumnas, Conexion.DBConnector dataBase,
+			JSONArray nodoEvaluar, String nodoHijoInsertarData) {
+		Map<String, String> tablaOwned = new HashMap<String, String>();
+		for (String objectos : listaDeColumnas) {
+			// INSERTANDO DATA
+			String nombreColumna = objectos.replaceAll("-", "_").replaceAll(":", "_");
+			if (nombreColumna.equals("uuid")) {
+				tablaOwned.put(nombreColumna, "varchar(50) primary key");
+			} else {
+				tablaOwned.put(nombreColumna, "MEDIUMTEXT");
+			}
+		}
+		tablaOwned.put("uuid_node", "varchar(250) , FOREIGN KEY  (uuid_node) REFERENCES exp_topology_node(uuid)");
+		try {
+			tablaOwnedNode = Util.crearTablasGenericoMap(dataBase, "exp_topology_owned_node_edgepoint", tablaOwnedNode,
+					tablaOwned);
+			DBRecord record = tablaOwnedNode.newRecord();
+			for (Object objetosNode : nodoEvaluar) {
+				// JSONOBJECT ES PARA TRAER UN OBJETO DEL JSON
+				JSONObject ownedNode = (JSONObject) objetosNode;
+				// QUIERO ATRAER EL UUID DE OWNEDNODE PARA LUEGO IMPLEMENTARLO EN LA BD
+				String columnaUuid = ownedNode.get("uuid").toString();
+				JSONArray listEdgePoint = ownedNode.getJSONArray(nodoHijoInsertarData);
+				for (Object objectEvaluado : listEdgePoint) {
+					JSONObject objetosEvaluadoDeJson = (JSONObject) objectEvaluado;
+					Map<String, Object> objetosMap = objetosEvaluadoDeJson.toMap();
+					record = tablaOwnedNode.newRecord();
+					for (Map.Entry<String, Object> entry : objetosMap.entrySet()) {
+						if (listaDeColumnas.stream().filter(x -> entry.getKey().equals(x)).findFirst().isPresent()) {
+							record.addField(entry.getKey().replaceAll("-", "_").replaceAll(":", "_"),
+									entry.getValue().toString());
+						} else {
+							record.addField(entry.getKey().replaceAll("-", "_").replaceAll(":", "_"), null);
+						}
+					}
+					record.addField("uuid_node", columnaUuid);
+					tablaOwnedNode.insert(record);
+
+				}
+			}
+		} catch (SQLException | ClassNotFoundException e) {
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
 
 }
--- src/main/java/util/Util.java
+++ src/main/java/util/Util.java
@@ -19,91 +19,102 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-public  class Util {
-    public static ObjetosPrincipales getObjetosPrincipales(String rutaDeArchivo) throws IOException {
-        ObjectMapper mapper = new ObjectMapper();
-        // CONFIGURAMOS LA CLASE MAPPER CON ALGUNOS PARAMETROS PARA EVITAR ERRORES DE SITAXIS
-        mapper.configure(JsonParser.Feature.ALLOW_UNQUOTED_FIELD_NAMES, true);
-        // FAIL_ON_UNKNOWN_PROPERTIES QUE FALLE SI NO ENCUENTRA PROPIEDADES ? FALSE -- NO, QUE SIGA
-        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        // ACCEPT_SINGLE_VALUE_AS_ARRAY QUE ACEPTE ARREGLOS -- TRUE, QUE SI ACEPTE
-        mapper.configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
+public class Util {
+	public static ObjetosPrincipales getObjetosPrincipales(String rutaDeArchivo) throws IOException {
+		ObjectMapper mapper = new ObjectMapper();
+		// CONFIGURAMOS LA CLASE MAPPER CON ALGUNOS PARAMETROS PARA EVITAR ERRORES DE
+		// SITAXIS
+		mapper.configure(JsonParser.Feature.ALLOW_UNQUOTED_FIELD_NAMES, true);
+		// FAIL_ON_UNKNOWN_PROPERTIES QUE FALLE SI NO ENCUENTRA PROPIEDADES ? FALSE --
+		// NO, QUE SIGA
+		mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+		// ACCEPT_SINGLE_VALUE_AS_ARRAY QUE ACEPTE ARREGLOS -- TRUE, QUE SI ACEPTE
+		mapper.configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
 
-        // LEER EL ARCHIVO JSON PARA ELLO USAMOS LA CLASE FILE QUE ME PIDE UNA RUTA DONDE ESTA EL ARCHIVO
-        File archivoJson = new File(rutaDeArchivo);
+		// LEER EL ARCHIVO JSON PARA ELLO USAMOS LA CLASE FILE QUE ME PIDE UNA RUTA
+		// DONDE ESTA EL ARCHIVO
+		File archivoJson = new File(rutaDeArchivo);
 
-        // PARA PODER TRANSFORMAR EL ARCHIVO A OBJETOS USAMOS EL READVALUE DE MAPPER, QUE ME PIDE EL ARCHIVO Y LA CLASE QUE ME SERVIRA PARA ALMACENAR LOS DATOS DEL ARCHIVO
-        ObjetosPrincipales principal = mapper.readValue(archivoJson, ObjetosPrincipales.class);
-        return principal;
-    }
-    // REUTILIZANDO NAME
-    public static String generarNames(List<Name> listaDeNames) {
-        String nameStringValue = "";
-        for (Name name: listaDeNames) {
-            nameStringValue = nameStringValue + name.getValue_name() + " : " + name.getValue() + " \n";
-        }
-        return nameStringValue;
-    }
+		// PARA PODER TRANSFORMAR EL ARCHIVO A OBJETOS USAMOS EL READVALUE DE MAPPER,
+		// QUE ME PIDE EL ARCHIVO Y LA CLASE QUE ME SERVIRA PARA ALMACENAR LOS DATOS DEL
+		// ARCHIVO
+		ObjetosPrincipales principal = mapper.readValue(archivoJson, ObjetosPrincipales.class);
+		return principal;
+	}
 
-    public static DBTable crearTablasGenerico(Conexion.DBConnector dataBase, String nombreTabla, DBTable tabla, String[][] fields ) throws SQLException, ClassNotFoundException {
-        //PASO 1 ELIMINAR LA TABLA ANTERIOR
-        tabla = dataBase.deleteTableIfExsist(nombreTabla);
-        tabla.createTable(fields);
-        return tabla;
-    }
-    public static DBTable crearTablasGenericoMap(Conexion.DBConnector dataBase, String nombreTabla, DBTable tabla, Map<String, String> fields) throws SQLException, ClassNotFoundException {
-        //PASO 1 ELIMINAR LA TABLA ANTERIOR
-        tabla = dataBase.deleteTableIfExsist(nombreTabla);
-        tabla.createTableMap(fields);
-        return tabla;
-    }
-    //TRANSFORMA A JSONOBJECT
-    public static JSONObject parseJSONFile(String filename) throws IOException {
-        String contenido = new String(Files.readAllBytes(Paths.get(filename)));
-        return new JSONObject(contenido);
-    }
+	// REUTILIZANDO NAME
+	public static String generarNames(List<Name> listaDeNames) {
+		String nameStringValue = "";
+		for (Name name : listaDeNames) {
+			nameStringValue = nameStringValue + name.getValue_name() + " : " + name.getValue() + " \n";
+		}
+		return nameStringValue;
+	}
 
-    public static JSONObject retonarListaPropiedadesAsociadasNodoHijo(JSONObject archivoJson, String nodoPadre, String nodoHijoPrimerSegmento ){
-        JSONObject propiedades = null;
-        try {
-            propiedades = archivoJson.getJSONObject(nodoPadre)
-                    .getJSONObject(nodoHijoPrimerSegmento);
+	public static DBTable crearTablasGenerico(Conexion.DBConnector dataBase, String nombreTabla, DBTable tabla,
+			String[][] fields) throws SQLException, ClassNotFoundException {
+		// PASO 1 ELIMINAR LA TABLA ANTERIOR
+		tabla = dataBase.deleteTableIfExsist(nombreTabla);
+		tabla.createTable(fields);
+		return tabla;
+	}
 
-        } catch (Exception exception) {
-            System.out.println("error:: " + exception.getMessage());
-        }
-         return propiedades;
-    }
+	public static DBTable crearTablasGenericoMap(Conexion.DBConnector dataBase, String nombreTabla, DBTable tabla,
+			Map<String, String> fields) throws SQLException, ClassNotFoundException {
+		// PASO 1 ELIMINAR LA TABLA ANTERIOR
+		tabla = dataBase.deleteTableIfExsist(nombreTabla);
+		tabla.createTableMap(fields);
+		return tabla;
+	}
 
-    public static List < String > listaDeColumnasPadreArray(JSONArray nodoConLasColumnas, String nodo) {
-        List < String > listaDeColumnas = new ArrayList < > ();
-        for (Object objetosNode: nodoConLasColumnas) {
-            JSONObject ownedNode = (JSONObject) objetosNode;
-            JSONArray listaDeEdgePoint = ownedNode.getJSONArray(nodo);
-            for (Object objetoEvaluado: listaDeEdgePoint) {
-                JSONObject objetoEvaluadoJson = (JSONObject) objetoEvaluado;
-                Map < String, Object > objectMap = objetoEvaluadoJson.toMap();
-                for (Map.Entry < String, Object > entry: objectMap.entrySet()) {
-                    listaDeColumnas.add(entry.getKey());
-                }
-            }
-        }
-        return listaDeColumnas;
+	// TRANSFORMA A JSONOBJECT
+	public static JSONObject parseJSONFile(String filename) throws IOException {
+		String contenido = new String(Files.readAllBytes(Paths.get(filename)));
+		return new JSONObject(contenido);
+	}
 
-    }
-    public static List < String > listaDeColumnasPadreObject(JSONObject nodoConLasColumnas, String nodo) {
-        List < String > listaDeColumnas = new ArrayList < > ();
-        JSONArray listaDeEdgePoint = nodoConLasColumnas.getJSONArray(nodo);
-        for (Object objetoEvaluado: listaDeEdgePoint) {
-            JSONObject objetoEvaluadoJson = (JSONObject) objetoEvaluado;
-            Map < String, Object > objectMap = objetoEvaluadoJson.toMap();
-            for (Map.Entry < String, Object > entry: objectMap.entrySet()) {
-                listaDeColumnas.add(entry.getKey());
-            }
-        }
+	public static JSONObject retonarListaPropiedadesAsociadasNodoHijo(JSONObject archivoJson, String nodoPadre,
+			String nodoHijoPrimerSegmento) {
+		JSONObject propiedades = null;
+		try {
+			propiedades = archivoJson.getJSONObject(nodoPadre).getJSONObject(nodoHijoPrimerSegmento);
 
-        return listaDeColumnas;
+		} catch (Exception exception) {
+			System.out.println("error:: " + exception.getMessage());
+		}
+		return propiedades;
+	}
 
-    }
+	public static List<String> listaDeColumnasPadreArray(JSONArray nodoConLasColumnas, String nodo) {
+		List<String> listaDeColumnas = new ArrayList<>();
+		for (Object objetosNode : nodoConLasColumnas) {
+			JSONObject ownedNode = (JSONObject) objetosNode;
+			JSONArray listaDeEdgePoint = ownedNode.getJSONArray(nodo);
+			for (Object objetoEvaluado : listaDeEdgePoint) {
+				JSONObject objetoEvaluadoJson = (JSONObject) objetoEvaluado;
+				Map<String, Object> objectMap = objetoEvaluadoJson.toMap();
+				for (Map.Entry<String, Object> entry : objectMap.entrySet()) {
+					listaDeColumnas.add(entry.getKey());
+				}
+			}
+		}
+		return listaDeColumnas;
+
+	}
+
+	public static List<String> listaDeColumnasPadreObject(JSONObject nodoConLasColumnas, String nodo) {
+		List<String> listaDeColumnas = new ArrayList<>();
+		JSONArray listaDeEdgePoint = nodoConLasColumnas.getJSONArray(nodo);
+		for (Object objetoEvaluado : listaDeEdgePoint) {
+			JSONObject objetoEvaluadoJson = (JSONObject) objetoEvaluado;
+			Map<String, Object> objectMap = objetoEvaluadoJson.toMap();
+			for (Map.Entry<String, Object> entry : objectMap.entrySet()) {
+				listaDeColumnas.add(entry.getKey());
+			}
+		}
+
+		return listaDeColumnas;
+
+	}
 
 }
